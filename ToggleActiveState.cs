0a700b71da9b305d461728efd5cf12e63ddecc92
YozoraKurage <130652890+YozoraKurage@users.noreply.github.com>
2024-05-04 22:16:39 +0900

    upload

---
+++ ToggleActiveState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class ToggleActiveState
+{
+    static ToggleActiveState()
+    {
+        SceneView.duringSceneGui += view =>
+        {
+            if (Event.current != null && Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.C)
+            {
+                foreach (var gameObject in Selection.gameObjects)
+                {
+                    gameObject.SetActive(!gameObject.activeSelf);
+                }
+            }
+        };
+    }
+}
